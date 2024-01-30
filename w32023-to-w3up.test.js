1ae55d8b63fd326ce98e06528b395e7737a75f00
bengo <171782+gobengo@users.noreply.github.com>
2024-01-29 16:19:51 -0800

    WIP parallel

--- w32023-to-w3up.test.js
+++ w32023-to-w3up.test.js
@@ -7,8 +7,10 @@ import * as CAR from "@ucanto/transport/car"
 import * as Client from '@ucanto/client'
 import * as ed25519 from '@ucanto/principal/ed25519'
 import * as Server from "@ucanto/server"
-import { ReadableStream } from 'stream/web'
+import { ReadableStream, TransformStream } from 'stream/web'
 import { migrate } from './migrate-w32023-to-w3up.js'
+import { Parallel } from 'parallel-transform-web'
+import { createServer } from 'http'
 
 /** example uploads from `w3 list --json` */
 const uploadsNdjson = `\
@@ -101,3 +103,154 @@ await test('can run migration to mock server', async () => {
   assert.ok(events.find(e => e.object.type.toLowerCase() === 'upload'))
   assert.ok(events.find(e => e.object.type.toLowerCase() === 'car'))
 })
+
+for (let concurrency = 1; concurrency <= 4; concurrency++) {
+  await test(`can map to store/add invocations in parallel concurrency=${concurrency}`, async () => {
+    let pulled = 0;
+    const createUploads = () => new globalThis.ReadableStream({
+      async pull(controller) {
+        console.log('start pull from upstream uploads', { pulled })
+        const upload = W32023Upload.from(uploadsNdjson.split('\n').filter(Boolean)[0])
+        pulled++
+        console.log('incremented pulled', pulled)
+        controller.enqueue(upload)
+      }
+    })
+    const delay = n => new Promise(resolve => setImmediate(() => resolve(n)))
+    /** @typedef {TransformStream} */
+    const parallelize = new Parallel(concurrency, delay)
+    const transformed = createUploads().pipeThrough(parallelize)
+    const pull1 = await take(transformed.getReader())
+    assert.equal(pulled, concurrency + 1)
+    console.log('pull1', pull1)
+    console.log('pulled', pulled)
+  })
+}
+
+/**
+ * transformer that transforms each input to N outputs as yielded by a
+ * fn (I) => AsyncIterable<O>
+ * @template I
+ * @template O
+ * @implements {Transformer<I, O>}
+ */
+class SpreadTransformer {
+  /**
+   * @param {(input: I) => AsyncIterable<O>} spread - convert an input to zero or more outputs
+   */
+  constructor(spread) {
+    this.spread = spread
+  }
+  async transform(input, controller) {
+    for await (const output of this.spread(input)) {
+      controller.enqueue(output)
+    }
+  }
+}
+
+await test(`can transform w32023upload using TransformAddResponseFromHttp`, { only: true }, async () => {
+  const concurrency = 1
+  const uploads = createEndlessUploads()
+  /** @type {Map<string,number>} */
+  const contentLengthForCarPart = new Map
+  const carFinder = createServer((req, res) => {
+    const lastPathSegmentMatch = req.url.match(/\/([^/]+)$/)
+    const cid = lastPathSegmentMatch && lastPathSegmentMatch[1]
+    if ( ! cid) {
+      res.writeHead(404)
+      res.end('cant determine cid');
+      return;
+    }
+    const contentLength = Math.floor(Math.random() * 100)
+    contentLengthForCarPart.set(cid, contentLength)
+    res.writeHead(200, {
+      "content-length": contentLength,
+    })
+    res.end()
+  })
+  carFinder.listen(0)
+  try {
+    const { url } = await listening(carFinder)
+    const transformerUploadToUploadPartResponse = new SpreadTransformer(
+      /**
+       * @param {W32023Upload} upload - upload whose parts should be fetched
+       */
+      async function * (upload) {
+        for (const part of upload.parts) {
+          const partUrl = new URL(`/ipfs/${part}`, url)
+          const response = await fetch(partUrl)
+          yield {
+            upload,
+            part,
+            response,
+          }
+        }
+      })
+    const transformStreamAddingResponse = new TransformStream(transformerUploadToUploadPartResponse)
+    const upload1 = await take(uploads.readable.getReader())
+    const writer1 = transformStreamAddingResponse.writable.getWriter()
+    try {
+      await writer1.ready
+      writer1.write(upload1)
+      // close endless uploads. we only want 1 here.
+      writer1.close()
+    } finally {
+      writer1.releaseLock()
+    }
+    // wrote one upload into transform.
+    // now try to read
+    const reader1 = transformStreamAddingResponse.readable.getReader()
+    const i1 = await reader1.read()
+    assert.ok(!i1.done)
+    assert.ok(i1.value)
+    assert.deepEqual(i1.value.upload, upload1)
+    assert.ok(i1.value.response instanceof Response)
+    assert.equal(i1.value.response.headers.get('content-length'), contentLengthForCarPart.get(i1.value.part))
+    assert.equal(typeof i1.value.part, 'string')
+  } finally {
+    carFinder.close()
+  }
+  assert.equal(uploads.pulledCount, concurrency + 1)
+})
+
+/** create an infinite stream of uploads */
+function createEndlessUploads() {
+  let pulledCount = 0
+  const readable = new globalThis.ReadableStream({
+    async pull(controller) {
+      const upload = W32023Upload.from(uploadsNdjson.split('\n').filter(Boolean)[0])
+      pulledCount++
+      console.log('incremented pulledCount', pulledCount)
+      controller.enqueue(upload)
+    }
+  })
+  return { get pulledCount() { return pulledCount }, readable }
+}
+
+/**
+ * @param {import('http').Server} server - server that should be listening on the returned url
+ */
+async function listening(server) {
+  const address = server.address()
+  if (typeof address === 'string') throw new Error(`unexpected address string`)
+  const { port } = address
+  const url = new URL(`http://localhost:${port}/`)
+  return { url }
+}
+
+/**
+ * take one item from a stream reader, then release the lock
+ * @template T
+ * @param {ReadableStreamDefaultReader<T>} reader - reader to read from
+ */
+async function take(reader) {
+  try {
+    const { done, value } = await reader.read()
+    if (done) {
+      throw new Error('EOF')
+    }
+    return value
+  } finally {
+    reader.releaseLock()
+  }
+}
